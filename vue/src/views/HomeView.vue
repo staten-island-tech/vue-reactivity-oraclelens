185f864bdc1bd0e99ef27311f1ef105022f64801
oraclelens <sophiar10200@gmail.com>
2023-03-09 13:14:49 -0500

    h

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script>
+import TheWelcome from "../components/TheWelcome.vue";
+
+export default {
+  name: "Home",
+  component: {},
+  data() {
+    return {
+      student: "help, i am so bad at algebra 2",
+      graduated: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else="graduated">loser</h1>
+  </main>
+</template>
